898bb0df242056d9847f6e3b6c499fe1a6665f81
woodsandpop <132575779+woodsandpop@users.noreply.github.com>
2023-07-30 07:03:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,5 @@ def chatbot():
         for message in messages:
             st.write(message["content"]) 
         st.audio(text_to_speech(system_response), format="audio/wav")
-
-
-
+        
 chatbot()
